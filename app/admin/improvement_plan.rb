e439ae2186cd35d44d2dbb9ebf9b0f670419ea11
Raghu Betina <raghu@firstdraft.com>
2018-02-28 15:20:23 +0000

    Whitelist columns in ImprovementPlan for ActiveAdmin

--- app/admin/improvement_plan.rb
+++ app/admin/improvement_plan.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ImprovementPlan do
 
+ permit_params :summary, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
