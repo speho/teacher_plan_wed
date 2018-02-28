4b9911bbfe203cc3869365e56647b66522e797d2
Raghu Betina <raghu@firstdraft.com>
2018-02-28 15:19:59 +0000

    Direct association between users and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :writer,
+             :class_name => "User",
+             :foreign_key => "user_id"
+
   belongs_to :improvement_plan
 
   # Indirect associations
