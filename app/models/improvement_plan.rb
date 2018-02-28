19de731866a9044b63c4b114255bf1257937c481
Raghu Betina <raghu@firstdraft.com>
2018-02-28 15:19:59 +0000

    Direct association between users and improvement_plans

--- app/models/improvement_plan.rb
+++ app/models/improvement_plan.rb
@@ -1,6 +1,10 @@
 class ImprovementPlan < ApplicationRecord
   # Direct associations
 
+  belongs_to :coach,
+             :class_name => "User",
+             :foreign_key => "user_id"
+
   has_many   :comments,
              :dependent => :destroy
 
